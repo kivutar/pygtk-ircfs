f950dbe6ac927a781e5bdd76a564bbb4579487c8
Jean-André Santoni <jeanandre.santoni@biblibre.com>
2010-12-08 16:30:32 +0100

    Scrollbar autoadjustment

--- src/pygtk-ircfs.py
+++ src/pygtk-ircfs.py
@@ -14,6 +14,7 @@ def Monitor(path, gui):
             f = open(path+'/out', 'r')
             gui.textbuffer.set_text(f.read())
             f.close()
+            gui.scrolltoend()
 
     wm = WatchManager()
     notifier = Notifier(wm, PModify())
@@ -28,30 +29,39 @@ class GUI():
     def __init__(self, path):
         self.path = path
 
-        self.tree       = gtk.glade.XML('main.glade')
-        self.window     = self.tree.get_widget("mainWindow")
-        self.textview   = self.tree.get_widget("mainTextView")
-        self.mainentry  = self.tree.get_widget("mainEntry")
-        self.sendbutton = self.tree.get_widget("sendButton")
+        self.tree           = gtk.glade.XML('main.glade')
+        self.window         = self.tree.get_widget("mainWindow")
+        self.scrolledwindow = self.tree.get_widget("mainScrolledWindow")
+        self.textview       = self.tree.get_widget("mainTextView")
+        self.mainentry      = self.tree.get_widget("mainEntry")
+        self.sendbutton     = self.tree.get_widget("sendButton")
 
         self.textbuffer = gtk.TextBuffer()
         self.textview.set_buffer(self.textbuffer)
         self.textbuffer.set_text('bar')
 
-        self.window.connect("destroy", self.destroy)
-        self.sendbutton.connect("clicked", self.send)
+        self.window.connect("destroy", self.on_destroy)
+        self.sendbutton.connect("clicked", self.on_send)
 
         self.window.show_all()
 
-    def destroy(self, widget, data=None):
+    def on_destroy(self, widget, data=None):
         gtk.main_quit()
 
-    def send(self, widget, data=None):
+    def on_send(self, widget, data=None):
         message = self.mainentry.get_text()
+        self.send_message(message)
+        self.mainentry.set_text('')
+
+    def send_message(self, message):
         f = open(path+'/in', 'w')
         f.write(message+'\n')
         f.close()
-        self.mainentry.set_text('')
+
+    def scrolltoend(self):
+        adjustment = self.scrolledwindow.get_vadjustment()
+        upper = adjustment.get_upper()
+        adjustment.set_value(upper)
 
     def main(self):
         gtk.gdk.threads_init()
